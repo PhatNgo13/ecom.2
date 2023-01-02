9db4e9381a85ea0187f88a33135bd75c15b4c5a9
Johnson Ngo <jngo11601@gmail.com>
2023-01-01 21:22:24 -0500

    updated

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ app.get("/albums", (req, res) => {
   });
 });
 
+//TELLING EXPRESS TO USE PATH 
 app.use(express.static(path.join(__dirname, "./client", "./build")));
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, "./client", "./build", "index.html"));
